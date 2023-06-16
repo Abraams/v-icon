0d457ca67aa4a77a60dff379deaf71859c88a123
abe.smile <abe.marchenko@gmail.com>
2023-06-16 14:29:04 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import VIcon from './components/VIcon.vue'
+import AppleIcon from '@reg-design/ds-static-icons/ds-icons/__apple/index'
+</script>
+
+<template>
+  <div>
+      <VIcon
+        width="300"
+        height="300"
+        color="#333"
+        :icon="AppleIcon"
+      />
+  </div>
+</template>
+
---
+++ src/components/VIcon.vue
@@ -0,0 +1,41 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        :width="width"
+        :height="height"
+        :viewBox="viewBox"
+        :fill="color"
+    >
+        <path
+            v-for="path in icon.svgPathData"
+            :fill-rule="path.fillRule"
+            :clip-rule="path.clipRule"
+            :d="path.d"
+        />
+    </svg>
+</template>
+
+<script setup>
+defineProps({
+    width: {
+        type: [String, Number],
+        default: 24
+    },
+    height: {
+        type: [String, Number],
+        default: 24
+    },
+    viewBox: {
+        type: String,
+        default: '0 0 24 24'
+    },
+    color: {
+        type: String,
+        default: '#333333'
+    },
+    icon: {
+        type: Object,
+        required: true
+    }
+})
+</script>
